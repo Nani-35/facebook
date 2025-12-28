44e7ed4b562c08e141a4647f9bab28964ee7ad79
Nani-35 <kovvurunaveenkumar705@gmail.com>
2025-12-28 21:35:43 +0530

    updated the file from master

--- DBUtilites.java
+++ DBUtilites.java
@@ -3,3 +3,4 @@ Hello guys
 this is a java file containing the info related to the db
 updated this file
 this is the change that i did in development branch dont switch to other before commiting the changes that we did in the current branch.
+This is a change from the master branch.
